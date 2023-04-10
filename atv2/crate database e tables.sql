8a4479e1b75c3cb514b7d8146da1cdb6e97e1b27
Maria Eduarda Pedroso <mpedroso@alunos.utfpr.edu.br>
2023-04-10 16:36:41 -0300

    Update crate database e tables.sql

--- atv2/crate database e tables.sql
+++ atv2/crate database e tables.sql
@@ -1,8 +1,8 @@
--- CRIAR BANCO DE DADOS
+-- CRIAR BANCO DE DADOS COM USUARIO POSTGREE
 -- create database atividade2;
 --------------------------------------------
 
--- CRIAR TABELAS
+-- CRIAR TABELAS COM USUARIO POSTGREE
 CREATE TABLE empregado (
 	SSN  int
 	,pnome varchar(20)
@@ -49,15 +49,21 @@ CREATE TABLE dependente (
 	,sexo varchar(1)
 );
 --------------------------------------------
--- CRIAR USUÁRIOS
-create user userX encrypted password '123';
-create user userA encrypted password '123';
-create user userB encrypted password '123';
-create user userC encrypted password '123';
-create user userD encrypted password '123';
-create user userE encrypted password '123';
+-- CRIAR USUÁRIOS COM USUARIO POSTGREE
+create user userA encrypted password '123'; -- pode recuperar ou modificar quaisquer relaçoes exceto dependentes e pode conceder privilégios para outros usuários. 
+create user userB encrypted password '123'; -- pode recuperar todos os atributos de empregado e departamento, exceto salario, o numero de seguro social do gerente e a data de inicio do gerente. 
+create user userC encrypted password '123'; -- pode recuperar ou modificar TRABALHA_EM, porem, pode apenas recuperar os atributos PNOME, INICIALM,UNOME e NUMEROD0SEGUROSOCIAl de EMPREGADO e os atributos NOMEP e NUMEROP de PROJETO 
+create user userD encrypted password '123'; -- pode recuperar qualquer atributo de EMPREGADO ou dependente e pode modificar DEPENDENTE. 
+create user userE encrypted password '123'; -- Pode recuperar qualquer atributo de EMPREGADO, porém, somente para as tuplas de EMPREGADO que possuam DNO = 3. 
 --------------------------------------------
 -- CONCEDER PRIVILÉGIOS
+-- RECUPERAR -> SELECT
+-- MODIFICAR -> UPDATE
+
+-- USAR VIEW NA DE IDO==3
+
+GRANT SELECT,XXX ON TO userA WITH GRANT OPTION;
+-- WITH GRANT OPTION PROPAGAR PRIVILÉGIOS
 
 --------------------------------------------
 -- INSERIR NA TABELA

4e73def182d8a43c1a3fd9708c593fea9e5be068
Maria Eduarda Pedroso <mpedroso@alunos.utfpr.edu.br>
2023-04-10 15:57:26 -0300

    Create crate database e tables.sql

---
+++ atv2/crate database e tables.sql
@@ -0,0 +1,8 @@
+create database atividade2;
+
+
+create user userA encrypted password '123';
+create user userB encrypted password '123';
+create user userC encrypted password '123';
+create user userD encrypted password '123';
+create user userE encrypted password '123';
